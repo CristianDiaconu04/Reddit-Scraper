c3a64599768ba3bcd7800c1fc071ef61c4795f49
Cristian <cristian.diaconu0423@gmail.com>
2024-05-06 22:50:20 -0400

    Added more post information scraping

--- main.py
+++ main.py
@@ -28,19 +28,53 @@ while running:
     scrapedSub = redditAPI.subreddit(subredditName)
 
     if sorting == "New":
-        posts = scrapedSub.new(limit=numPosts)
+        posts = list(scrapedSub.new(limit = numPosts))
     elif sorting == "Hot":
-        posts = scrapedSub.hot(limit=numPosts)
+        posts = list(scrapedSub.hot(limit = numPosts))
     elif sorting == "Top":
-        posts = scrapedSub.top(limit=numPosts)
+        posts = list(scrapedSub.top(limit = numPosts))
     elif sorting == "Controversial":
-        posts = scrapedSub.controversial(limit=numPosts)
+        posts = list(scrapedSub.controversial(limit = numPosts))
 
 
     # Print post titles
     print(f"\nTop {numPosts} {sorting} posts from r/{subredditName}:\n")
+    counter = 0
     for post in posts:
-        print("Post Title: ", post.title)
+        print(f"Post Title {counter}: ", post.title)
+        counter += 1
+
+    # Ask user if they want more information about a certain post
+    # Get more information about the post if required
+    correctNum = False
+    while (not correctNum):
+        postInterest = input("Would you like more information about any of the posts? Enter its number, or no: ")
+        if postInterest == "no":
+            break
+        else:
+            postInterest = int(postInterest)
+            if postInterest < 1 or postInterest > numPosts:
+                print(f"Your post number is out of bounds, enter a number from 0 to {numPosts}.")
+                continue
+            else:
+                print(f"Post {postInterest} Title: {posts[postInterest].title}")
+                # Output the post body if there is one
+                if posts[postInterest].selftext:
+                    print("-------------------------------")
+                    print(f"{posts[postInterest].selftext}")
+                    print("-------------------------------")
+                print(f"Post {postInterest} Author: {posts[postInterest].author}")
+                print(f"Post {postInterest} # of Upvotes: {posts[postInterest].score}")
+                print(f"Post {postInterest} # of Comments: {posts[postInterest].num_comments}")
+                print(f"Post {postInterest} awards received: {posts[postInterest].total_awards_received}")
+                print(f"Post {postInterest} NSFW: {posts[postInterest].over_18}")
+
+                # Get the top 10 comments
+                comments = posts[postInterest].comments[:10]
+                print(f"The top comments of post {postInterest} are: ")
+                for comment in comments:
+                    print(f"{comment.body}")
+
 
     again = input("\nWould you like to scrape another subreddit? (yes/no) ").strip().lower()
     if again != 'yes':
