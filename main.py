583a84b4620edc81b043c1e5dae8bd0220be22f2
Cristian Diaconu <cristian.diaconu0423@gmail.com>
2024-05-06 21:29:42 -0400

    Got the basic scraper working

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import praw 
-from sorting import Sorting
+import praw
 
 # Reddit API
-reddit = praw.Reddit(
+redditAPI = praw.Reddit(
     client_id='wsmpPsB8AQWfgcYHgS8IyQ',
     client_secret='j6Sm_4n1Oy9XVMM59POsnZwO68EW9g',
-    user_agent='MyRedditScraper',  # Describe your bot with a user agent
+    user_agent='MyRedditScraper',
 )
 
 running = True
@@ -16,23 +15,34 @@ while running:
     
     # Get the sorting type
     while incorrectSorting:
-        sorting = input("What would you like to sort by? Options are New, Hot, Top, or Controversial: ") # Has to be one of New, Hot, Top, Controversial
-        if sorting == "New" or sorting == "Hot" or sorting == "Top" or sorting == "Controversial":
+        sorting = input("What would you like to sort by? (new/hot/top/controversial) ").strip().capitalize() 
+        if sorting in ["New", "Hot", "Top", "Controversial"]:
             incorrectSorting = False
         else:
             print("Incorrect choice")
 
     numPosts = input("How many posts would you like? ")
+    numPosts = int(numPosts)
 
+    # This is the variable of the actual subreddit
+    scrapedSub = redditAPI.subreddit(subredditName)
 
     if sorting == "New":
-        
+        posts = scrapedSub.new(limit=numPosts)
     elif sorting == "Hot":
-
+        posts = scrapedSub.hot(limit=numPosts)
     elif sorting == "Top":
-
+        posts = scrapedSub.top(limit=numPosts)
     elif sorting == "Controversial":
+        posts = scrapedSub.controversial(limit=numPosts)
 
-    
 
+    # Print post titles
+    print(f"\nTop {numPosts} {sorting} posts from r/{subredditName}:\n")
+    for post in posts:
+        print("Post Title: ", post.title)
+
+    again = input("\nWould you like to scrape another subreddit? (yes/no) ").strip().lower()
+    if again != 'yes':
+        running = False
 
